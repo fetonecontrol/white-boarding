de4c7050afea7cbcb3a14be7c0e2969ed87f487c
Frederick Ernest <frederick.ernest@gmail.com>
2020-09-04 11:48:30 -0700

    work on problem 3

--- test.js
+++ test.js
@@ -8,18 +8,44 @@ const urlMaker = (x) => {
 
 const string = "Jasmine Ann Jones";
 const loop = (e, i) => {
-  e = string.split("");
-  if (i >= 10) {
+  if (i >= e.length) {
     //e = string.join("");
+
     return e;
-  } else if (e[i] === " "){
-      e.splice(i, "%20");
-      console.log(e[i]);
-      i++;
-      return loop(e, i);
   } else {
-    console.log(e[i]);
+    e = string.split('');
+    if (e[i] === ' '){
+      e.splice(i, 1, '%20');
+      console.log(e);
       i++;
       return loop(e, i);
+    }
+    i++;
+    return loop(e, i);
   }
 }
+
+//problem 2
+
+//with 2 for loops
+let input = [7, 9, "hi", 12, "hi", 7, 53];
+const noDupes = (e) => {
+  for (let i = 0; i < e.length; i++){
+    for (let j = 0; j < e.length; j++){
+      if (i === j){
+      e.pop(i, 1);
+      }
+    }
+    
+    console.log(e[i]);
+    return e;
+  }
+}
+
+//with filter
+const filterDupes = (e) =>{
+  const x = e.filter(e => e !== e);
+}
+
+//problem 3
+
