1b8720726ab6868f8a996a1211f8499df8be48fe
Frederick Ernest <frederick.ernest@gmail.com>
2020-09-04 10:14:11 -0700

    use single quotes for special characters in js

--- test.js
+++ test.js
@@ -1,8 +1,25 @@
 // problem 1
 // solution 1 this works.
-const string = "Jasmine Ann Jones";
 
-const urlMaker = (e) => {
+const urlMaker = (x) => {
   x = string.split(" ").join("%20");
-    return x;
+  return x;
+}
+
+const string = "Jasmine Ann Jones";
+const loop = (e, i) => {
+  e = string.split("");
+  if (i >= 10) {
+    //e = string.join("");
+    return e;
+  } else if (e[i] === " "){
+      e.splice(i, "%20");
+      console.log(e[i]);
+      i++;
+      return loop(e, i);
+  } else {
+    console.log(e[i]);
+      i++;
+      return loop(e, i);
+  }
 }
